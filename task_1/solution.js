3b7bddd24a84c346bd80df7cd05bd8357736aca4
Lika Vysotskaya <superlika1992@gmail.com>
2022-09-28 13:28:41 +0300

    11

--- task_1/solution.js
+++ task_1/solution.js
@@ -46,7 +46,7 @@ function addInCartSumChange(sum, delta) {
 
     // Задание №1.3. Увеличение суммы в корзине
 
-    let newSum = oldSum + difference;
+    let newSum = sum + delta;
     let newSumText = newSum + ' ₽';
     console.log(newSum)
 
